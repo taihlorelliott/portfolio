c1f8cf29a94c59aca12996dca73f1b5c5090d769
Taihlor Elliott <taihlor.elliott@gmail.com>
2024-12-03 10:59:21 -0500

    inline styling

--- Taihlor-Portfolio/src/App.jsx
+++ Taihlor-Portfolio/src/App.jsx
@@ -13,16 +13,44 @@ const App = () => {
     {text: 'Learn CSS', done: true, comfort: 8},
     {text: 'Master React', done: false, comfort: 6},
   ];
+  
+  const heading = {
+    color: 'Yellow',
+    fontSize: '40px',
+    textAlign: 'center',
+    position: 'absolute', top: 0, left: 0, width: '100%'
+}
+const subtitle = {
+  color: 'lightBlue',
+  position: 'absolute', top: 60, left: 0, width: '100%',
+  textAlign: 'center'
+}
+const headingTwo = {
+  color: 'Yellow',
+  position: 'absolute', top: 80, left: 0, width: '100%',
+  textAlign: 'center'
+}
+const ulist = {
+  color: 'lightBlue',
+  listStyleType: 'none',
+  position: 'absolute', top: 130, left: 0, width: '90%',
+  textAlign: 'center'
+}
+
+const list = {
+  textDecoration: 'none'
+}
+
 
   return (
     <>
-      <h1>Hello, Welcome to Taihlor's Portfolio!</h1>
-      <p>{language.text}</p>
+      <h1 style={heading}>Hello, Welcome to Taihlor's Portfolio!</h1>
+      <p style={subtitle}>{language.text}</p>
       
-      <h2>Frontend/Backend Languages:</h2>
-      <ul>
+      <h2 style={headingTwo}>Frontend/Backend Languages:</h2>
+      <ul style={ulist}>
         {languages.map((language, index) => 
-          <li key={index}>
+          <li style={list} key={index}>
             {language.done ? `Task Completed - ${language.text} - Comfort Level: ${language.comfort}`  : language.text }
           </li>
         )}
